9ab8eb2a7bac980a6727791896276742acdf33b0
CrabBerjoget <ketam.berjoget@gmail.com>
2026-01-19 22:25:58 +0800

    Update exclusion.ps1

--- exclusion.ps1
+++ exclusion.ps1
@@ -1,4 +1,15 @@
 cls
+# ================== AUTO ADMIN ELEVATION ==================
+if (-not ([Security.Principal.WindowsPrincipal] `
+    [Security.Principal.WindowsIdentity]::GetCurrent()
+).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+
+    Start-Process powershell `
+        -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" `
+        -Verb RunAs
+    exit
+}
+
 [Console]::InputEncoding = [System.Text.Encoding]::UTF8
 
 # ================== UI HELPERS ==================
